cd8505b99f3818437c23b770814a919aa1c11bba
fxliang <fx.liang@outlook.com>
2026-08-01 20:23:01 +0800

    librime: add a patch to deploy para

--- src/Rules/LibRime.hs
+++ src/Rules/LibRime.hs
@@ -45,8 +45,10 @@ librimeRule = do
             cmd_ (Cwd librimePredictSrc) "git apply ../patches/librime-predict.patch"
             -- remove absolute path by __FILE__ macro; tabs selectable; refactor tabs
             cmd_ (Cwd src) "git checkout ."
+            cmd_ (Cwd src) "git clean -xdf"
             cmd_ (Cwd src) "git apply ../patches/librime.patch"
             cmd_ (Cwd src) "git apply ../patches/librime-perf-syllabifier-cache-repeated-QuerySpelling-iterat.patch"
+            cmd_ (Cwd src) "git apply ../patches/librime-perf-deploy-compile-independent-dictionaries-in-para.patch"
             -- in-memory sorted cache for user dictionary queries
             cmd_ (Cwd src) "git apply ../patches/librime-userdict-cache.patch",
           cmakeFlags = \BuildEnv {..} ->
